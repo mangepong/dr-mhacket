e244a41c84f836ed92f4cf0ddf018e7fd3c29e44
mangepong <emil_mange@hotmail.com>
2021-05-09 12:33:59 +0200

    asd commmit

--- src/app/start/start.service.ts
+++ src/app/start/start.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +9,12 @@ import { map } from 'rxjs/operators';
 export class StartService {
 
   constructor(
-        private router: Router,
-        private http: HttpClient
+        // private router: Router,
+        // private http: HttpClient
     ) { }
 
 
-  APIcall(deposit) {
-      return this.http.post(`https://project-api.mangepongjs.me/deposit`, {deposit: deposit});
-  }
+  // APIcall(deposit) {
+  //     return this.http.post(`https://project-api.mangepongjs.me/deposit`, {deposit: deposit});
+  // }
 }
